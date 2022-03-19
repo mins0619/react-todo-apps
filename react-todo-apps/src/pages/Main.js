af0846c4a25aa82618e6aa151705102230e5c12f
mins0619 <mins5588@naver.com>
2022-03-19 15:54:52 +0900

    react-router-dom 을 이용하여 투두 list 페이지 분할

---
+++ react-todo-apps/src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+const Main = () => {
+    return (
+      <div>
+        <h1>홈</h1>
+        <p>투두리스트로 입장</p>
+        <Link to="/todo">투두</Link>
+      </div>
+    );
+  };
+  
+  export default Main;
---
+++ react-todo-apps/src/pages/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Todo = () => {
+
+    
+    return (
+        <div>
+            <p>투두리스트 페이지</p>
+            <Link to="/">돌아가기</Link>
+        </div>
+        
+        );
+};
+
+export default Todo;
